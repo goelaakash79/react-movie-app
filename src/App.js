b31f79e26d67676f57b6b9f53c707c6200d81db1
goelaakash79 <goelaakash79@gmail.com>
2018-12-29 19:11:41 +0530

    props done

--- src/App.js
+++ src/App.js
@@ -2,29 +2,74 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import Header from './components/Header';
 import Search from './components/Search';
+import Movie from './components/Movie';
 import Footer from './components/Footer';
 
 class App extends Component {
 
-  getMovie = (e) => {
+  state = {
+    movies: [
+      {
+        movie_name: 'Koi mil gya',
+        rating: '8.1',
+        imdb_id: 'tt04d7e1'
+      },
+      {
+        movie_name: 'Welcome',
+        rating: '8.6',
+        imdb_id: 'tt040201'
+      },
+      {
+        movie_name: 'Judwaa',
+        rating: '5.1',
+        imdb_id: 'tt04d587'
+      }
+    ]
+  }
+
+  handleClick = (e) => {
+    // console.log(e.target);
+    this.setState({
+      movie_name: 'Batman v/s Superman',
+      rating: '9.8',
+      imdb_id: 'tt8751dc'
+    });
+  }
+
+  handleMouseOver = (e) => {
+    console.log(e.pageX);
+  }
+
+  handleOnCopy = (e) => {
+    console.log("Stop copying");
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      movie_name: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
     e.preventDefault();
-    const movie = e.target.elements.movie.value;
-    if (movie) {
-      axios.get('http://www.omdbapi.com/?apikey=thewdb&s='+movie)
-      .then((res) => {
-        console.log(res);
-      });
-    } else return;
+    console.log("form submitted", this.state.movie_name);
   }
 
   render() {
     return (
       <div className="App">
+        <button className="btn btn-outline-dark" style={{color: "#333"}} onClick={this.handleClick}>Click Here</button>
+        <h1 onMouseOver={this.handleMouseOver}>hover here</h1>
+        {this.state.movies[0].movie_name}
+        <p onCopy={this.handleOnCopy}>{this.state.movie_name}<br/>{this.state.rating}</p>
+        <hr/>
+        <form className="form-group" onSubmit={this.handleSubmit}>
+          <input className="form-control" type="text" onChange={this.handleChange}/>
+          <br/>
+          <button className="btn btn-outline-info">Submit</button>
+        </form>
+        <Movie movies={this.state.movies}/>
 
-
-        <Header/>
-        <Search getMovie={this.getMovie}/>
-        <Footer/>
       </div>
     );
   }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Movie = () => {
-  return (
-    <div>
-      <p>Movie</p>
-    </div>
-  );
+class Movie extends Component{
+  render(){
+    console.log(this.props);
+    const { movies } = this.props;
+    const movieList = movies.map(movie => {
+      return (
+        <div className="movie card col-sm-3">
+          <div>Movie: {movie.movie_name}</div>
+          <div>Rating: {movie.rating}</div>
+          <div>IMDB Id: {movie.imdb_id}</div>
+        </div>
+      )
+    });
+    return (
+      <div className="movie-list row col-lg-12">
+        { movieList }
+      </div>
+    )
+  }
 }
 
 export default Movie;
